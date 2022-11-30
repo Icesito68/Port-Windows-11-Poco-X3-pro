90845fe38c72529522d8c0ef926e716791f2e08b
Haouari haitam Kouider <57036855+haouarihk@users.noreply.github.com>
2022-11-30 13:34:49 +0100

    Create validator.sh
    Last layer of defense before initiating rm 28,
    This will let the user check if his configuration is gonna work, or he missed a step. and running rm 28.
    will turn his phone into an expensive paperweight

---
+++ Utils/validator.sh
@@ -0,0 +1,17 @@
+# Just makes sure that parted is in sbin and ready
+cp ./parted /sbin/
+
+chmod -R 777 /sbin/parted
+
+# retrives the line data for 
+number=$(parted /dev/block/sda -s print all | grep -w "userdata" | awk '{print $1}')
+
+# Checks if its 28th
+if [ $number -eq 28 ]
+ then
+    echo "All good"
+else
+    echo "Recovery should be at 28, But its at $number"
+    echo "GO BACK AND FOLLOW THE GUIDE CORRECTLY, YOU MISSED THE RENUMBERING STEP"
+    echo "CONTINUING WILL BREAK YOUR RECOVERY, And by that YOUR PHONE TOO"
+fi
